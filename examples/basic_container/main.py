70c46f0d207aaf653b6b15447fdaad98666abc59
lbabetto <l.babetto@cineca.it>
2024-12-03 11:31:10 +0100

    Add function to process lists of files

--- examples/basic_container/main.py
+++ examples/basic_container/main.py
@@ -1,9 +1,14 @@
 import argparse
+import sys
 from src.process import process_data
+from src.process import process_files
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--input', type=str)
-    parser.add_argument('--output', type=str)
-    args = parser.parse_args()
-    process_data(args.input, args.output)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--input", type=str)
+    # parser.add_argument("--output", type=str)
+    # args = parser.parse_args()
+    # process_data(args.input, args.output)
+
+    input_files = sys.argv[1:]
+    process_files(input_files)
--- examples/basic_container/src/process.py
+++ examples/basic_container/src/process.py
@@ -1,6 +1,17 @@
 import pandas as pd
+from os.path import splitext, basename
+
 
 def process_data(infile, outfile):
     df = pd.read_parquet(infile)
     df.to_json(outfile, orient="records")
-    
+
+
+def process_files(input_list):
+    for input in input_list:
+        df = pd.read_parquet(input)
+
+        output = basename(input)
+        output = splitext(output)[0]
+        output = f"/output/{output}.json"
+        df.to_json(output, orient="records")
